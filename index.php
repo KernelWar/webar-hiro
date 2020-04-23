aeee5b180b075d551336a8be82b9f431173a2cba
Jose Lopez <josesocrateslopez@gmail.com>
2020-04-23 14:37:23 -0500

    se agrego: favicon

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="shortcut icon" type="image/png" href="realidad-aumentada.png"/>
+  <title>WebAR-Hiro</title>
   <script src="aframe/aframe.min.js"></script>
   <script src="aframe/aframe-layout-component.min.js"></script>
   <script src="aframe/aframe-ar.js"></script>
